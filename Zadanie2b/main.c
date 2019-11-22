38c250e6a55dc3e94d8459036a5821f9ecac8418
Tomek <pantomko@gmail.com>
2019-11-22 19:48:38 +0100

    up2

--- Zadanie2b/main.c
+++ Zadanie2b/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int get_pessel_sum(char * p)
+{
+    return 9*(p[0]-48) + 7*(p[1]-48) + 3*(p[2]-48) + (p[3]-48) + 9*(p[4]-48)
+              + 7*(p[5]-48) + 3*(p[6]-48) + (p[7]-48) + 9*(p[8]-48) + 7*(p[9]-48);
+}
+
 int is_pesel_valid(char * p)
 {
     for(int i = 0; i < 11; i++)
         if(!(p[i] >= '0' && p[i] <= '9')) return 0;
 
-    int sum = 9*(p[0]-48) + 7*(p[1]-48) + 3*(p[2]-48) + (p[3]-48) + 9*(p[4]-48)
-              + 7*(p[5]-48) + 3*(p[6]-48) + (p[7]-48) + 9*(p[8]-48) + 7*(p[9]-48);
+    int sum = get_pessel_sum(p);
 
     printf("%d : ", (sum%10));
     return ((sum%10) == (p[10]-48));
@@ -15,15 +20,15 @@ int is_pesel_valid(char * p)
 
 int main()
 {
-    char pesel_1[11] = "44051401358";
-    char pesel_2[11] = "99062324429";
-    char pesel_3[11] = "85072358457";
-    char pesel_4[11] = "79103184247";
+    const char pesel_1[11] = "44051401358";
+    const char pesel_2[11] = "99062324429";
+    const char pesel_3[11] = "85072358457";
+    const char pesel_4[11] = "89121909667";
 
     printf("PESEL 44051401358 : %d\n", is_pesel_valid(pesel_1));
     printf("PESEL 99062324429 : %d\n", is_pesel_valid(pesel_2));
     printf("PESEL 85072358457 : %d\n", is_pesel_valid(pesel_3));
-    printf("PESEL 79103184247 : %d\n", is_pesel_valid(pesel_4));
+    printf("PESEL 89121909667 : %d\n", is_pesel_valid(pesel_4));
 
     return 0;
 }
--- Zadanie4b/main.c
+++ Zadanie4b/main.c
@@ -1,13 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-char * (int day, int month, int year, char gender)
+int get_pessel_sum(char * p)
 {
+    int sum = 9*(p[0]-48) + 7*(p[1]-48) + 3*(p[2]-48) + (p[3]-48) + 9*(p[4]-48)
+              + 7*(p[5]-48) + 3*(p[6]-48) + (p[7]-48) + 9*(p[8]-48) + 7*(p[9]-48);
+    return sum;
+}
 
+char* generate_password(char day[3], char month[3], char year[5], char gender)
+{
+    srand(time(0));
+
+    int decade = 0;
+    char year_2[5], month_2[2];
+    memcpy(year_2, year, 4);
+
+    char *tab = (char*)malloc(12* sizeof(char));
+
+    memcpy(tab, year+2, 2);
+    memcpy(tab+4, day, 2);
+
+    if(atoi(year) < 1900) decade = 8;
+    else
+    {
+        memset(year_2, '0', 1);
+        decade = (atoi(year) - 1900) / 100;
+        sprintf(month_2, "%d", atoi(month)+(decade*20));
+        memcpy(tab+2,month_2, 2);
+    }
+
+    tab[6] = (rand() % 10) + 48;
+    tab[7] = (rand() % 10) + 48;
+    tab[8] = (rand() % 10) + 48;
+
+    if( gender == 'K' ) while( (tab[9] = rand() % 10) % 2 == 0);
+    else while( (tab[9] = rand() % 10) % 2 != 0);
+    tab[9] += 48;
+    tab[10] = 'n';
+
+    tab[10] = (get_pessel_sum(tab)%10)+48;
+
+
+
+    tab[11] = '\0';
+    return tab;
 }
 
 int main()
 {
-    printf("Hello world!\n");
+    const char* pessel = generate_password("12", "11", "1922", 'M');
+
+    printf("%s", pessel);
     return 0;
 }
