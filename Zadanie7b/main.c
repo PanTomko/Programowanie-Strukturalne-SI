571f5d8da22f87a87b3ab467c96347493d17243c
Tomek <pantomko@gmail.com>
2019-11-29 18:07:36 +0100

    changes to zad 7
    Changes based of zad9

--- Zadanie7b/main.c
+++ Zadanie7b/main.c
@@ -3,21 +3,61 @@
 #include <string.h>
 #include <limits.h>
 
+void swap(int * n1, int * n2)
+{
+    int tmp = *n1;
+    *n1 = *n2;
+    *n2 = tmp;
+}
+
+void quick_sort( int *tab, int left, int right )
+{
+    int i = left, j = right;
+    int x = tab[( left + right ) / 2 ];
+    do
+    {
+        while( tab[ i ] < x )
+             i++;
+
+        while( tab[ j ] > x )
+             j--;
+
+        if( i <= j )
+        {
+            swap( &tab[ i ], &tab[ j ] );
+            i++;
+            j--;
+        }
+    } while( i <= j );
+
+    if( right > i ) quick_sort( tab, i, right );
+    if( left < j ) quick_sort( tab, left, j );
+
+}
+
 int main()
 {
-    unsigned int min = UINT_MAX, max = 0, tmp, res;
+    unsigned int count = 0, current_szie = 0, tmp, res;
+    unsigned int * numbers = NULL;
     for(;;)
     {
         res = scanf("%d", &tmp);
-        if(res != 1)
+        if(res < 1)
         {
-            printf("Max -> %d\n", max);
-            printf("Min -> %d\n", min);
+            quick_sort(numbers, 0, count-1);
+            printf("Min -> %d\n", numbers[0]);
+            printf("Max -> %d\n", numbers[count-1]);
             break;
         }
 
-        if(max < tmp) max = tmp;
-        if(min > tmp) min = tmp;
+        count += 1;
+        if( count > current_szie ){
+            numbers = realloc(numbers, sizeof(unsigned int) * (current_szie+10) );
+            current_szie+=10;
+        }
+
+        numbers[count-1] = tmp;
+
     }
 
     return 0;
